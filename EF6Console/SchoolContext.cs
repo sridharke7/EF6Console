3e7bfac0c5c0f7760911629704a751dd129ed53e
Sridhar Kesavan <sridhar.kesavan@outlook.com>
2019-07-10 17:50:45 +0900

    Add project files.

---
+++ EF6Console/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6Console
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolDb")
+        {
+            Database.SetInitializer<SchoolContext>(new DropCreateDatabaseIfModelChanges<SchoolContext>());
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
